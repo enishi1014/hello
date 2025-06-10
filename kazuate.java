9998a0c651116768dc73722722ccfc91a7957d25
enishi1014 <enishi0365@gmail.com>
2025-06-10 09:03:28 +0900

    クラス名、ファイル名を変更しました

--- kazuate.java
+++ kazuate.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class kazuate {
+public class Kazuate {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
